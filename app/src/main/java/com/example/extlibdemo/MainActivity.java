f95b4eb509c667957baed86ec68fedbae5af09c8
Erik Holmberg <b21eriho@student.his.se>
2022-05-16 09:09:13 +0200

    Utilize picasso

--- app/src/main/java/com/example/extlibdemo/MainActivity.java
+++ app/src/main/java/com/example/extlibdemo/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.extlibdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = findViewById(R.id.image);
+
+        Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(image);
     }
 }
